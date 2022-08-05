dd02ac16003ec16c0ee24837fcbac587d9796c39
27akash <akashprajapati27@gmail.com>
2022-08-05 22:04:13 +0530

    Updates

---
+++ precache-manifest.9011fefaba88c0643c24e412a1ec5b13.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd055997ef081f94149c",
+    "url": "/static/css/main.c1ad3579.chunk.css"
+  },
+  {
+    "revision": "dd055997ef081f94149c",
+    "url": "/static/js/main.a6daf259.chunk.js"
+  },
+  {
+    "revision": "b0af9cb635d3de1e5ab2",
+    "url": "/static/js/runtime~main.f400fc0c.js"
+  },
+  {
+    "revision": "dc3e41ec9ea08ee01350",
+    "url": "/static/css/2.7ecf3deb.chunk.css"
+  },
+  {
+    "revision": "dc3e41ec9ea08ee01350",
+    "url": "/static/js/2.185852a6.chunk.js"
+  },
+  {
+    "revision": "2c126976528df378765e",
+    "url": "/static/js/3.bacbc41e.chunk.js"
+  },
+  {
+    "revision": "d4a25617e64a23774165234bb8797d48",
+    "url": "/static/media/BannerImg_1.d4a25617.jpg"
+  },
+  {
+    "revision": "4a3940057f0465ab4c604375df3290e1",
+    "url": "/static/media/BannerImg_2.4a394005.jpg"
+  },
+  {
+    "revision": "b596cece9c75812d4aa1594d9df8bfc8",
+    "url": "/static/media/BannerImg_3.b596cece.jpg"
+  },
+  {
+    "revision": "3017703a61632b2716e4a13549701dd7",
+    "url": "/static/media/contactImage.3017703a.jpg"
+  },
+  {
+    "revision": "e81efba2416ad2f74964e1aa50de1f51",
+    "url": "/index.html"
+  }
+];
